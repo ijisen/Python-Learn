02db88d742aeae3d0ac65877c3a6e4fcd85fd804
jisen <471535104@163.com>
2017-02-10 17:50:57 +0800

    add note

---
+++ if.py
@@ -0,0 +1,8 @@
+age = 25;
+if age > 20 :
+	print('Too Old');
+else :
+	print('Too Small');
+name = 25;
+
+print(name)
---
+++ input.py
@@ -0,0 +1,3 @@
+name = input('please input your name: ');
+
+print('Welcome to Python word: ' , name)
