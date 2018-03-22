23b2090b7bb00bcff43f74248664081e93af557b
ijisen <471535104@163.com>
2018-03-22 16:10:49 +0800

    add note

--- calc.py
+++ calc.py
@@ -1,1 +1,4 @@
-print(100+200)
+w = input('please input w: ');
+h = input('please input h: ');
+total = 5*10;
+print(w,'*',h,'=',int(w)*int(h));
--- if.py
+++ if.py
@@ -4,5 +4,14 @@ if age > 20 :
 else :
 	print('Too Small');
 name = 25;
-
+# alert name
 print(name)
+
+gender = input('please input your sex : ')
+
+if gender == 'girl' :
+	print('hello beatifull girl')
+elif gender == 'boy' :
+	print('hello boy')
+else :
+	print('i don\'t konw who you are !')
